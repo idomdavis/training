af2277c9f9260312562736359bb3e10768f91d19
Dom Davis <dom@domdavis.com>
2017-10-17 08:58:02 +0100

    Provide variadic and slice examples

---
+++ exercise-3/slice/fizzbuzz.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+const (
+	fizz = "Fizz"
+	buzz = "Buzz"
+)
+
+func main() {
+	fizzbuzz([]int{1, 2, 4, 5, 6, 7, 8, 15, 21, 42})
+}
+
+func fizzbuzz(values []int) {
+	for _, i := range values {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Printf("%-5d - %s%s", i, fizz, buzz)
+		case i%3 == 0:
+			fmt.Printf("%-5d - %s", i, fizz)
+		case i%5 == 0:
+			fmt.Printf("%-5d - %s", i, buzz)
+		default:
+			fmt.Print(i)
+		}
+
+		fmt.Println()
+	}
+}
