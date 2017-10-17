b4e13317a485bfa3617b940ce1d71dbefa322382
Dom Davis <dom@domdavis.com>
2017-10-17 09:00:26 +0100

    Update examples

--- exercise-5/fizzbuzz.go
+++ exercise-5/fizzbuzz.go
@@ -11,20 +11,26 @@ const (
 )
 
 func main() {
-	for _, i := range []int{1, 2, 4, 5, 6, 7, 8, 15, 21, 42} {
-		println(i, fizzbuzz(i))
+	for k, v := range fizzbuzz(1, 2, 4, 5, 6, 7, 8, 15, 21, 42) {
+		println(k, v)
 	}
 }
 
-func fizzbuzz(i int) string {
-	switch {
-	case i%3 == 0 && i%5 == 0:
-		return fmt.Sprintf("%s%s", fizz, buzz)
-	case i%3 == 0:
-		return fizz
-	case i%5 == 0:
-		return buzz
-	default:
-		return strconv.Itoa(i)
+func fizzbuzz(values ...int) map[int]string {
+	m := make(map[int]string, len(values))
+
+	for _, i := range values {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			m[i] = fmt.Sprintf("%s%s", fizz, buzz)
+		case i%3 == 0:
+			m[i] = fizz
+		case i%5 == 0:
+			m[i] = buzz
+		default:
+			m[i] = strconv.Itoa(i)
+		}
 	}
+
+	return m
 }
